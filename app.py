e96a5e7ee3b7fe186d0674499edddd35211511ed
Kidzonio <luciolinsneto@gmail.com>
2025-04-27 21:11:48 -0300

    Code formatting style has been updated

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ import logging;
 
 class SystemMonitorApp (QWidget):
 
-    def __init__ (self):
+    def __init__(self):
 
-        super ().__init__ ();
+        super().__init__();
         
         self.target_language = 'en'; # pt for portuguese
         
-        self.translator = Translator ();
+        self.translator = Translator();
         
         self.translations = {
             'CPU Usage': '',
@@ -21,45 +21,45 @@ class SystemMonitorApp (QWidget):
             'Used Memory': '',
         };
         
-        self.update_translations ();
-        self.initUI ();
+        self.update_translations();
+        self.initUI();
         
-        self.signals = WorkerSignals ();
-        self.signals.cpu_usage.connect (self.update_cpu_label);
-        self.signals.memory_info.connect (self.update_memory_label);
+        self.signals = WorkerSignals();
+        self.signals.cpu_usage.connect(self.update_cpu_label);
+        self.signals.memory_info.connect(self.update_memory_label);
         
-        self.worker = Worker (self.signals);
-        self.worker.start ();
+        self.worker = Worker(self.signals);
+        self.worker.start();
         
-    def initUI (self):
+    def initUI(self):
         
-        self.setWindowTitle (self.translations['Memory']);
-        self.setGeometry (100, 100, 400, 200);
+        self.setWindowTitle(self.translations['Memory']);
+        self.setGeometry(100, 100, 400, 200);
         
-        layout = QVBoxLayout ();
+        layout = QVBoxLayout();
         
-        self.cpu_label = QLabel (self.translations['CPU Usage'] + ': N/A', self);
-        layout.addWidget (self.cpu_label);
+        self.cpu_label = QLabel(self.translations['CPU Usage'] + ': N/A', self);
+        layout.addWidget(self.cpu_label);
         
-        self.memory_label = QLabel (self.translations['Memory'] + ': N/A', self);
-        layout.addWidget (self.memory_label);
+        self.memory_label = QLabel(self.translations['Memory'] + ': N/A', self);
+        layout.addWidget(self.memory_label);
         
-        self.setLayout (layout);
+        self.setLayout(layout);
     
-    def update_translations (self):
+    def update_translations(self):
 
-        for key in self.translations.keys ():
+        for key in self.translations.keys():
             try:
-                self.translations[key] = self.translator.translate (key, dest=self.target_language).text;
+                self.translations[key] = self.translator.translate(key, dest=self.target_language).text;
             except Exception as e:
                 logging.error(f"Error translating text '{key}' to language '{self.target_language}': {e}")
                 self.translations[key] = key;
     
     def update_cpu_label(self, cpu_percent):
 
-        self.cpu_label.setText (f"{self.translations['CPU Usage']}: {cpu_percent}%");
+        self.cpu_label.setText(f"{self.translations['CPU Usage']}: {cpu_percent}%");
     
-    def update_memory_label (self, memory_info):
+    def update_memory_label(self, memory_info):
 
         total_memory, available_memory, used_memory = memory_info;
         self.memory_label.setText (
@@ -70,5 +70,5 @@ class SystemMonitorApp (QWidget):
     
     def closeEvent(self, event):
 
-        self.worker.stop ();
-        event.accept ();
+        self.worker.stop();
+        event.accept();
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import sys;
-from PyQt5.QtWidgets import QApplication;
+from PyQt5.QtWidgets import QApplication, QMessageBox;
 from app import SystemMonitorApp;
 
-if __name__ == "__main__":
-    app = QApplication (sys.argv);
-    ex = SystemMonitorApp ();
-    ex.show ();
-    sys.exit (app.exec_ ());
+def main():
+
+    app = QApplication(sys.argv);
+
+    try:
+        ex = SystemMonitorApp();
+        ex.show();
+        sys.exit(app.exec_());
+    except Exception as error:
+        error_box = QMessageBox();
+        error_box.critical(None, 'Application Error', f'An error ocurred:\n{str(error)}');
+        sys.exit(1);
+
+
+if __name__ == '__main__':
+    main();
